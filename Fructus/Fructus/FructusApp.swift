abcc2a96d4fae226295322bd46e700d07d36d5ab
sftwrgmrmom <michelle.lane@snhu.edu>
2021-09-12 14:52:13 -0400

    Start of Projects

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Michelle Lane on 9/12/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
